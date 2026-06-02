e4038df67e312713c572fd3949c2bd1b0f306765
junoeasy <thswnsgh00@naver.com>
2026-06-02 09:46:35 +0900

    vault backup: 2026-06-02 09:46:35

--- exercises/test_mujoco.py
+++ exercises/test_mujoco.py
@@ -1,5 +1,6 @@
 import gymnasium as gym
 import time
+import numpy as np
 env = gym.make("InvertedPendulum-v5", render_mode="human")
 
 obs, info = env.reset()
@@ -8,12 +9,12 @@ print("obs shape:", obs.shape)
 print("action space:", env.action_space)
 
 for step in range(10000):
-    if obs[1]<-0.1:
-        action=[-1.0]
-    elif obs[1]>0.1:
-        action=[1.0]
-    else:
-        action=[0.0]
+    cart_pos, pole_angle, cart_vel, pole_ang_vel = obs
+    action_value = 10.0 * pole_angle + 1.0 * pole_ang_vel
+    action_value = np.clip(action_value, -3.0, 3.0)
+
+    action = np.array([action_value], dtype=np.float32)
+
     obs, reward, terminated, truncated, info = env.step(action)
 
     print(step, reward, terminated, truncated,info,obs[0],obs[1],obs[2],obs[3])
